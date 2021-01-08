6b90f8d25c9966d243947976061f0361351fb744
roro1053 <c1053rroro@gmail.com>
2021-01-08 10:43:01 +0900

    コントローラにアクションを定義

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.includes(:user).order('created_at DESC')
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    tag_list = params[:post][:tag_ids].split(',')
+    if @post.save
+      @post.save_tags(tag_list)
+      flash[:success] = '投稿しました!'
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:content)
+    end
+
+    def tag_params
+      params.require(:post).permit(:words)
+    end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   has_many  :tag_relationships, dependent: :destroy
   has_many  :tags, through: :tag_relationships
+
+  
 end
