3ff787bc7e032a29c92aeaa5657d3cb91622e880
roro1053 <c1053rroro@gmail.com>
2021-01-17 21:05:59 +0900

    フォームを一覧ページに移動

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show,:edit,:update]
 
   def index
+    @post = Post.new
     @posts = Post.includes(:user).order('created_at DESC')
   end
 
